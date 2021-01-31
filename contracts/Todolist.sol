5d4c7841777983e0a4987f21443790301c322020
thurendous <thurendous2@gmail.com>
2021-01-31 13:36:29 +0900

    daily grind part

--- contracts/Todolist.sol
+++ contracts/Todolist.sol
@@ -4,4 +4,23 @@ contract Todolist {
     // state varible is written into the blockchain. It represents the state of the smart contract on the blockchain.
     // we use public then solidity gives us a function to read the varible.
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id; // uint: unsigned integer
+        string content;
+        bool completed;
+    }
+    // put it in the storage of the blockchain.
+    mapping(uint256 => Task) public tasks; // store a key-value pair -> like a database for us on chain.
+
+    // access the contract for the first time we have a data inside. This is the reason of constructor function.
+    constructor() public {
+        createTask("Check out dapp.com");
+    }
+
+    // create a function to write the "Task" strct into the mapping
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
---
+++ src/app.js
@@ -0,0 +1,67 @@
+App = {
+  loading: false,
+  contracts: {},
+
+  load: async () => {
+    await App.loadWeb3();
+    await App.loadAccount();
+    await App.loadContract();
+    // await App.render();
+  },
+
+  // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
+  loadWeb3: async () => {
+    if (typeof web3 !== "undefined") {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+      window.alert("Please connect to Metamask.");
+    }
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      window.web3 = new Web3(ethereum);
+      try {
+        // Request account access if needed
+        await ethereum.enable();
+        // Acccounts now exposed
+        // web3.eth.sendTransaction({});
+      } catch (error) {
+        // User denied account access...
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = web3.currentProvider;
+      window.web3 = new Web3(web3.currentProvider);
+      // Acccounts always exposed
+      // web3.eth.sendTransaction({/* ... */});
+    }
+    // Non-dapp browsers...
+    else {
+      console.log(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  },
+
+  loadAccount: async () => {
+    // Set the current blockchain account
+    App.account = web3.eth.accounts[0];
+    console.log(App.account);
+  },
+
+  loadContract: async () => {
+    // create a JavaScript version of the smart contract
+    const todoList = await $.getJSON("TodoList.json");
+    App.contracts.TodoList = TruffleContract(todoList); //send this todolist json into the truffle contract function to be eaten
+    App.contracts.TodoList.setProvider(App.web3Provider);
+    console.log(todoList);
+  },
+};
+
+$(() => {
+  $(window).load(() => {
+    App.load();
+  });
+});
